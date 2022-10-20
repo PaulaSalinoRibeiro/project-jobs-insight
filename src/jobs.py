4f9e00527502655727c467c03a27b761dc3b9921
Paula <paulasalinor@gmail.com>
2022-10-20 14:59:59 -0300

    feat: add jobs function

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,6 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path) as file:
+        jobs = csv.DictReader(file)
+        return list(jobs)
