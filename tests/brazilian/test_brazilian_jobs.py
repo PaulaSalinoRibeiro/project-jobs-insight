6b580decf08b2647b460449d1e3de2db8b2e4f23
Paula <paulasalinor@gmail.com>
2022-10-20 19:23:48 -0300

    feat: test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    dict_translet_mock = {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
+
+    assert dict_translet_mock in read_brazilian_file(
+        "tests/mocks/brazilians_jobs.csv"
+    )
