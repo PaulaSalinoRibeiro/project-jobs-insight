6f1264c5d8d3c9ee34b0ff7e6b7b6f6b2c01d2eb
Paula <paulasalinor@gmail.com>
2022-10-25 08:12:15 -0300

    fix: data in jobs mock test_sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 def test_sort_by_criteria():
     jobs = [
-        {"min_salary": 1200, "max_salary": 6000, "date_posted": "2021-10-21"},
+        {"min_salary": 1200, "max_salary": 6000, "date_posted": "2021-01-10"},
         {"min_salary": 1600, "max_salary": 3000, "date_posted": "2022-10-21"},
     ]
 
